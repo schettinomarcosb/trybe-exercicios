2ab7b68eb932cc72483112a21cf932c2c18e133d
Marcos Schettino <schettinomarcosb@gmail.com>
2022-04-07 12:14:52 -0300

    exercicios 4.2

---
+++ modulo-1-fundamentos/bloco-4-javascript-inicio/dia-2/exercicios-4.2.js
@@ -0,0 +1,42 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for(let number of numbers) {
+//   console.log(number);
+// }
+ let sum = 0;
+// for (let index = 0; index < numbers.length; index +=1) {
+//   sum += numbers[index];
+// }
+// console.log(sum);
+// let arithmeticAverage = 0;
+// for (let index = 0; index < numbers.length; index +=1) {
+//   sum += numbers[index];
+//   arithmeticAverage = sum/numbers.length;
+// }
+// console.log(arithmeticAverage);
+// if (arithmeticAverage > 20) {
+//   console.log("valor maior que 20");
+// }
+// else {
+//   console.log("valor menor ou igual a 20");
+// }
+// let numbersAscending = numbers.sort();
+// console.log(numbersAscending);
+// console.log(numbersAscending[numbersAscending.length-1};
+// console.log(numbersAscending[numbersAscending[0]);
+// let odds = 0;
+// for (let index = 0; index < numbers.length; index +=1) {
+//   if (numbers[index] % 2 == 1) {
+//     odds += 1;
+//   }
+// }
+// if (odds == 0) {
+//   console.log ("nenhum valor ímpar encontrado");
+// }
+// else {
+//   console.log (odds);
+// }
+let array = [];
+for (let index = 1; index <= 25; index += 1) {
+array.push(index/2);
+}
+console.log(array);
