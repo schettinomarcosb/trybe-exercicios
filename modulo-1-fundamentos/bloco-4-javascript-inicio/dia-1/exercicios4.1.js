0c04c4c8949c269d8803e8b07772138ebf40b9d0
Marcos Schettino <schettinomarcosb@gmail.com>
2022-04-07 20:15:22 -0300

    exercicios 4.1 até nº9

--- modulo-1-fundamentos/bloco-4-javascript-inicio/dia-1/exercicios4.1.js
+++ modulo-1-fundamentos/bloco-4-javascript-inicio/dia-1/exercicios4.1.js
@@ -1,9 +1,8 @@
-const a = -120;
-const b = 30;
-const c = 30;
-let maior;
-let sinal;
-let triAngle;
+const a = 3;
+const b = 4;
+const c = 4;
+// let maior;
+// let sinal;
 // console.log(a+b);
 // console.log(a-b);
 // console.log(a*b);
@@ -22,26 +21,83 @@ let triAngle;
 //     maior = "os números são iguais";
 // }
 // console.log(maior);
-// switch (triAngle) {
-//   case (a < 0):
-//     console.log("erro");
-//     break;
-//   case b < 0:
-//     triAngle = "erro";
-//     break;
-//   case c < 0:
-//     triAngle = "erro";
-//     break;
-//   case a+b+c == 180:
-//     triAngle = "true";
-//   default:
-//     triAngle = "false";
+// if (a < 0 || b < 0 || c < 0) {
+//   console.log("erro");
 // }
-// console.log(triAngle);
-let bishop;
-let pawn;
-let knight;
-let rook;
-let queen;
-let king;
-let 
+// else if (a+b+c === 180) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+// let bishop;
+// let pawn;
+// let knight;
+// let rook;
+// let queen;
+// let king;
+// let piece = "pawn";
+// switch(piece.toLowerCase()) {
+//   case "bishop":
+//     console.log("diagonal");
+//     break;
+//   case "pawn":
+//     console.log("1 space at time - only sides and forward")
+//     break;
+//   case "knight":
+//     console.log("movement in shape of an \"l\"")
+//     break;
+//   case "rook":
+//     console.log("no limit of distance, but can't move in diagonals")
+//     break;
+//   case "queen":
+//     console.log("can be moved at any direction and any distance")
+//     break;
+//   case "king".toLowerCase():
+//     console.log("can be moved at any direction, 1 space each round")
+//     break;
+//   default:
+//     console.log("erro")
+//     break;
+// }
+// let nota = 132;
+// if (nota < 0 || nota >100) {
+//   console.log("nota inválida");
+// }
+// else if (nota >= 90) {
+//   console.log("A")
+// }
+// else if (nota >= 90) {
+//   console.log("A")
+// }
+// else if (nota >= 80) {
+//   console.log("B")
+// }
+// else if (nota >= 70) {
+//   console.log("C")
+// }
+// else if (nota >= 60) {
+//   console.log("D")
+// }
+// else if (nota >= 50) {
+//   console.log("E")
+// }
+// else {
+//   console.log("F")
+// }
+// let even;
+// if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+//   even = true;
+// }
+// else {
+//   even = false;
+// }
+// console.log(even)
+let odd;
+if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) {
+  odd = true;
+}
+else {
+  odd = false;
+}
+console.log(odd)
