73b0e04271671c3fb302e4bd0a58ad3c855a4c64
Marcos Schettino <schettinomarcosb@gmail.com>
2022-04-07 19:34:30 -0300

    correções exercicios 4.2

--- modulo-1-fundamentos/bloco-4-javascript-inicio/dia-2/exercicios-4.2.js
+++ modulo-1-fundamentos/bloco-4-javascript-inicio/dia-2/exercicios-4.2.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // for(let number of numbers) {
 //   console.log(number);
 // }
- let sum = 0;
+//  let sum = 0;
 // for (let index = 0; index < numbers.length; index +=1) {
 //   sum += numbers[index];
 // }
@@ -19,10 +19,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // else {
 //   console.log("valor menor ou igual a 20");
 // }
-// let numbersAscending = numbers.sort();
-// console.log(numbersAscending);
-// console.log(numbersAscending[numbersAscending.length-1};
-// console.log(numbersAscending[numbersAscending[0]);
+// let highest = 0;
+// for(let index = 0; index < numbers.length; index+=1) {
+//   if (numbers[index] > highest) {
+//     highest = numbers[index]
+//   }
+// }
+// console.log(highest)
 // let odds = 0;
 // for (let index = 0; index < numbers.length; index +=1) {
 //   if (numbers[index] % 2 == 1) {
@@ -35,6 +38,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // else {
 //   console.log (odds);
 // }
+// let lowest = 999999;
+// for(let index = 0; index < numbers.length; index+=1) {
+//   if (numbers[index] < lowest) {
+//     lowest = numbers[index]
+//   }
+// }
+// console.log(lowest)
 let array = [];
 for (let index = 1; index <= 25; index += 1) {
 array.push(index/2);
